45b1b4d7dfc3088861ec63abb7f37f975b3fd31d
Jacob McSwain <jmcswain@hashicorp.com>
2020-09-29 18:17:41 -0500

    Update

--- main.tf
+++ main.tf
@@ -1,31 +1,15 @@
-locals {
-  ttl_default = 300
-  a_ttl_default = local.ttl_default
-  aaaa_ttl_default = local.ttl_default
-  cname_ttl_default = local.ttl_default
-  mx_ttl_default = local.ttl_default
-  ns_ttl_default = local.ttl_default
-  txt_ttl_default = local.ttl_default
-  srv_ttl_default = local.ttl_default
-  all_domain_records = csvdecode(file("${path.module}/domains.csv"))
+provider "aws" {
+  region  = "us-east-1"
+}
 
-  tags = {
-    Environment = "Production"
-    Name = "Domain CSV Test"
-  }
-
-  domain_rows = [
-    for r in local.all_domain_records:
-    r
-    if lower(r.row_type) == "domain"
-  ]
+module "private_layer"{
+  source = "./private_layer"
 }
 
 resource "aws_route53_zone" "csv" {
-  for_each = { for r in local.domain_rows : replace(r.domain, ".", "_") => r }
+  for_each = { for r in [] : replace(r.domain, ".", "_") => r }
   vpc {
     vpc_id = "vpc-1234"
   }
   name = each.value.domain
-  tags = local.tags
 }
---
+++ private_layer/outputs.tf
@@ -0,0 +1,7 @@
+output "local_all_domain_records" {
+    value = local.all_domain_records
+}
+
+output "local_domain_rows" {
+    value = local.domain_rows
+}
